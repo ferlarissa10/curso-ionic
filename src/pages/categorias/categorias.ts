390142e5168c64128ae3b4a0cef9f0a7e7e1e485
ferlarissa10 <fernandalarissahtp@gmail.com>
2018-05-30 22:26:33 -0300

    mostrando lista de categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,3 +1,5 @@
+import { API_CONFIG } from './../../config/api.config';
+import { CategoriaDto } from './../../models/categoria.dto';
 import { CategoriaService } from './../../services/domain/categoria.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -15,6 +17,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDto[];
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams, 
@@ -24,7 +28,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
           .subscribe(response => {
-            console.log(response);
+            this.items = response;
           },
           error => {
             console.log(error);
