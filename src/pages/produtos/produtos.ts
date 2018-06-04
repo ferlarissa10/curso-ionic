0fa8899ba04e5410101460f8e36e1d77d9258897
ferlarissa10 <fernandalarissahtp@gmail.com>
2018-06-04 11:26:06 -0300

    componente infinit scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -18,7 +18,8 @@ import { ProdutoDto } from '../../models/produto.dto';
 })
 export class ProdutosPage {
 
-  items: ProdutoDto[];
+  items: ProdutoDto[] = [];
+  page: number = 0;
 
   constructor(
     public navCtrl: NavController, 
@@ -34,19 +35,23 @@ export class ProdutosPage {
    loadData(){
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe(response => {
-        this.items = response['content'];
+        let start = this.items.length;
+        this.items = this.items.concat(response['content'])
+        let end = this.items.length -1
+        console.log(this.page)
+        console.log(this.items)
         loader.dismiss();
-        this.loadImageUrls();
+        this.loadImageUrls(start, end);
       },
       error => {
         loader.dismiss();
       });
    }
 
-   loadImageUrls() {
-      for (var i=0; i<this.items.length; i++) {
+   loadImageUrls(start:number, end : number) {
+      for (var i=start; i<=end; i++) {
         let item = this.items[i];
         this.produtoService.getSmallImageFromBucket(item.id)
           .subscribe(response => {
@@ -55,6 +60,16 @@ export class ProdutosPage {
           error => {});
       }
     } 
+    /*loadImageUrls() {
+      for (var i=0; i<this.items.length; i++) {
+        let item = this.items[i];
+        this.produtoService.getSmallImageFromBucket(item.id)
+          .subscribe(response => {
+            item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+          },
+          error => {});
+      }
+    } */
 
     showDetail(produto_id : string){
       this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id})
@@ -69,10 +84,19 @@ export class ProdutosPage {
     }
 
     doRefresh(refresher) {
+      this.page = 0;
+      this.items = [];
       this.loadData()
       setTimeout(() => {
         refresher.complete();
       }, 1000)
+    }
+    doInfinite(infiniteScroll) {  
+      this.page ++;
+      this.loadData();
+      setTimeout(() => {
+        infiniteScroll.complete();
+      }, 1000);
     }
 
 }
